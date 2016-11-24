18d8bbb77ff593fd3b8a33322c0835a0e37dc0f9
WilliamAlvelos <will.alvelos@gmail.com>
2016-11-24 13:55:04 -0200

    Commit catraca ainda nao totalmente funcional

---
+++ Assets/catracaMove.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class catracaMove : MonoBehaviour {
+	public int speed;
+
+	private bool abre = true;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//transform.Translate(new Vector3(0, 0, Time.deltaTime*speed));
+
+		if (abre) {
+			this.transform.GetChild (0).gameObject.transform.Rotate (new Vector3 (Time.deltaTime * speed * -9, 0, 0));
+			if(this.transform.GetChild (0).gameObject.transform.rotation.x >= 0.70){
+				abre = false;
+			}
+
+			Debug.Log (this.transform.GetChild (0).gameObject.transform.rotation.x);
+
+		} else {
+
+			this.transform.GetChild (0).gameObject.transform.Rotate (new Vector3 (Time.deltaTime * speed * 9, 0, 0));
+			if (this.transform.GetChild (0).gameObject.transform.rotation.x <= 0.50) {
+				abre = true;
+			}
+		}
+	}
+}
